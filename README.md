a69d420423f7bfd56ad497b9573f4c05020e3e63
frikky <frikky@dev.wsg.int>
2017-06-07 18:25:25 +0100

    Made a PoC work. Should be easy from here

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # MongoDB docker
-Testing mongoDB in docker for storage later
+MongoDB and rest API to use as threat intel for different SI(E)Ms. Addon for QRadar is available in Intelligencefeed. Needs implementation with mongoDB thoo
+
+# Todo? 
+* Add logic option for json or raw format (not xml :)). Depends whats the best.
+* Add logic for URLs and hashes. 
+* Make proper category lookup work. 
+* Add extensions management and script management for parsing of external sources.
+* Add docker configuration for the flask server, and remove for mongodb. Only used now for fast cleanups.
+* Make it able to automatically add data to the DB. On demand load or timebased?
+
+*** When above is done - Create 
+* Start using API tokens both in the database and the flask server. Not a rush as it's ran locally per now. 
+* Go away localhost :^)
+* More config ability \o/
 
 # MongoDB logic
 |client| -> webserver API -> mongoDB -> webserver -> |client|
@@ -13,9 +26,9 @@ Return:
             "ip": "192.168.1.0"
             "id": counter?
             "containers": [{
-		"category": "c2", 	# IP is part of a Bot C&C
-		"name": zeus, 		# IP was found in the zeus logs	
-		"id": aksdjalskjdalkj 	# Databsae ID for quicker lookups
+				"category": "c2", 		# IP is part of a Bot C&C
+				"name": zeus, 			# IP was found in the zeus logs	
+				"id": aksdjalskjdalkj 	# Databsae ID for quicker lookups
 	    }],
             "addeddate": datetime.datetime.utcnow(),
             "modifieddate": datetime.datetime.utcnow()
--- flask/server.py
+++ flask/server.py
@@ -1,61 +1,130 @@
 #!flask/bin/python
-from pymongo import MongoClient
-from flask import Flask, jsonify, request
+
+import json
 import datetime
+from sys import argv
+from werkzeug import Response
+from pymongo import MongoClient
 from mongodb import database_handler
+from bson import ObjectId, json_util
+from flask import Flask, request, abort
 
 app = Flask(__name__)
+serverip = '127.0.0.1'
+serverport = 27017
+
+# BSON to JSON parser
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (datetime.datetime, datetime.date)):
+            return obj.isoformat()
+        elif isinstance(obj, ObjectId):
+            return unicode(obj)
+        return json.JSONEncoder.default(self, obj)
+
+# Rewriting flasks jsonify function with the above class in mind
+def jsonify(*args, **kwargs):
+    return Response(json.dumps(dict(*args, **kwargs), cls=JSONEncoder), \
+        mimetype='application/json')
 
 class correlate_data(database_handler):
 	def __init__(self, ip, port):
-		self.database = database_handler(ip, port)
+            self.database = database_handler(ip, port)
 
 	# Returns error messages
 	def default_error(self, data="", path=""):
-		if not data or not path:	
-			return {'error': 'Can\'t find the information you\'re looking for'}
+            if not data or not path:	
+                return {'error': 'Can\'t find the information you\'re looking for'}
 
-		return {'error': 'Can\'t find %s in collection %s' % (data, path)}
+            return {'error': 'Can\'t find %s in collection %s' % (data, path)}
 
-		# Do mongodb stuff
+        # Do mongodb stuff
 	def get_data(self, collection, path, data):
-		data = self.database.find_object(collection, data)
+            resp = ""
 
-		print data.count()
-		return self.default_error(path, data)
+            # Add url and ip
+            if path == "ip":
+                resp = self.database.find_ip_object(collection["ips"], data)
 
-serverip = '172.28.3.163'
-serverport = 80
+            # Verifies after basic categories 
+            if not resp:
+                categories = self.database.get_available_category_collections()
+
+                # Needs reverse path check to IP, URL or HASH.
+                if path in categories:
+                    print "TESTING"
+                    return "HELLLOOOOOO"
+                    # Lookup other shit
+
+            # Check categories here
+
+            if resp is None or not resp:
+                return self.default_error(path, resp)
+
+            return resp 
+
+        def generate_data(self):
+            db = self.database.mongoclient.ip
+            category_db = self.database.mongoclient.category
+
+            ip = "192.168.0.1\n"
+            category = "c2"
+            name = "zeus"
+
+            # Generate other data first to append to the IP-range
+            ip = ip[:-1]
+            data = self.database.add_new_ip(db, db.ips, category_db, ip, \
+                category=category, name=name)
+
+            return data
+
 find_data = correlate_data(serverip, serverport)
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return jsonify(find_data.default_error())
+    return jsonify(find_data.default_error())
 
 @app.route('/', methods=['GET'])
 def standard():
-	return jsonify(find_data.default_error())
+    return jsonify(find_data.default_error())
 
 @app.route('/', methods=['POST'])
 def add_task():
-	# Verify if the host is part of the API subscribers 
-	data = {"error": "OMG SOMETHING WENT WRONG"} ## ADD STUFF
-	return jsonify(data)
+    # Verify if the host is part of the API subscribers 
+    data = {"error": "OMG SOMETHING WENT WRONG"}
+    return jsonify(data)
+
+
+@app.route('/categories', methods=['GET'])
+def get_categories():
+    data = {"categories": []}
+    for category in find_data.database.get_available_category_collections():
+        data["categories"].append(category)
+
+    return jsonify(data)
 
 @app.route('/<string:path>/<string:task>', methods=['GET'])
 def get_tasks(path, task):
-	db = find_data.database.mongoclient
-	data = find_data.get_data(db.ip[path], path, task)
+    find_data.generate_data()
 
-	print request.headers["host"]
+    db = find_data.database.mongoclient[path]
+    data = find_data.get_data(db, path, task)
 
-	if len(task) == 0:
-		abort(404)
+    if len(data) == 0:
+        abort(404)
 
-	return jsonify(data)
+    try:
+        tmp_ret = data
+    except TypeError:
+        return jsonify({"error": "Python fault error in jsonifying data."})
 
-
-# ip, category, timeadded, timemodified
+    try:
+        return jsonify(tmp_ret)
+    except ValueError:
+        return jsonify({"error": "%s is not a valid path." % path}) 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    try:
+        app.run(debug=True, threaded=True, port=int(argv[1]))
+    except IndexError:
+        app.run(debug=True, threaded=True)
